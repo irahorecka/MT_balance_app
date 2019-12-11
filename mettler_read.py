f48f1bded2289923ace7901fc99e19fc9faf1764
Ira Horecka <ira89@icloud.com>
2019-12-10 22:26:57 -0800

    adjust for the pi

--- mettler_read.py
+++ mettler_read.py
@@ -39,8 +39,8 @@ class Scale:
 
 
 def main():
+    scale_val = Scale()
     while True:
-        scale_val = Scale()
         byte_val = scale_val.get_value()
         str_val = byte_val.decode_to_str(byte_val)
         print(str_val)
